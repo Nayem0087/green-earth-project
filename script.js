12de1a4c3bba4a12111df8e057cdb6bd2d97d11c
Nayem Ahmed <nayemk0087@gmail.com>
2026-03-06 15:33:31 +0600

    added my dynamical all tree card

--- script.js
+++ script.js
@@ -6,23 +6,61 @@
 //         .Catch((e) => console.log(e)); //get find error
 
 const categoriesContainer = document.getElementById('categoriesContainer');
-
+// dynamic btn
 async function loadCategories() {
-        // async await
-        const res = await fetch('https://openapi.programming-hero.com/api/categories')
-        const data = await res.json();
-        console.log(data);
-        console.log(categoriesContainer);
-        data.categories.forEach(category => {
-            console.log(category);
-            const btn = document.createElement('button');
-            btn.className = 'btn btn-outline w-full';
-            btn.textContent = category.category_name;
-            categoriesContainer.appendChild(btn);
-        })
+    // async await
+    const res = await fetch('https://openapi.programming-hero.com/api/categories')
+    const data = await res.json();
+    // console.log(data);
+    // console.log(categoriesContainer);
+    data.categories.forEach(category => {
+        // console.log(category);
+        const btn = document.createElement('button');
+        btn.className = 'btn btn-outline w-full';
+        btn.textContent = category.category_name;
+        categoriesContainer.appendChild(btn);
+    })
+}
+// dynamic trees section
+// load trees
+async function loadTrees() {
+    const res = await fetch('https://openapi.programming-hero.com/api/plants');
+    const data = await res.json();
+    displayTrees(data.plants);
+}
+
+// display trees
+function displayTrees(trees) {
+    // console.log(trees);
+    trees.forEach(tree => {
+        // console.log(tree);
+        const card = document.createElement('div');
+        card.className = 'card bg-white shadow-sm';
+        card.innerHTML = `
+            <figure>
+                <img src="${tree.image}" 
+                alt="${tree.name}" 
+                title="${tree.name}" 
+                class="h-40 w-full object-cover"
+                />
+            </figure>
+                <div class="card-body p-2">
+                <h2 class="card-title">${tree.name}</h2>
+                <p class="line-clamp-2">A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                <!-- badge -->
+                <div class="badge badge-success badge-outline">${tree.category}</div>
+                <div class="flex justify-between items-center">
+                <h2 class="font-bold text-xl text-[#4ade80]">${tree.price}</h2>
+                <button class="btn btn-primary">Buy Now</button>
+                </div>
+                </div>
+    `;
+    treesContainer.appendChild(card);
+    })
 }
 
 loadCategories();
+loadTrees();
 
 
 
